5fcaa29ea7f20f70293c03745b844e12262f7b95
Samuel <odunlamisamuel4@gmail.com>
2023-05-02 09:32:45 +0100

    new edit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,7 +6,7 @@
  *
  * Return: the number of nodes
  */
-size_t print_listint(const listint_t *h)
+size_t print_listint(const listint_t* h)
 {
 	size_t num = 0;
 
@@ -16,6 +16,6 @@ size_t print_listint(const listint_t *h)
 		num++;
 		h = h->next;
 	}
-  
+
 	return (num);
 }
